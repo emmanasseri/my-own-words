863ec4921972b08f379d6b075a0a8abe2bba5fd7
emmanasseri <enasseri02@gmail.com>
2024-10-20 04:31:41 -0700

    insufficient funds error still but abt to do a lot ot stuff

--- server/services/mintAndRegisterIP.js
+++ server/services/mintAndRegisterIP.js
@@ -2,88 +2,97 @@ const { ethers } = require("ethers");
 const { toHex } = require("viem");
 const {
   AddressZero,
-  IpMetadata,
   PIL_TYPE,
-  RegisterIpAndAttachPilTermsResponse,
   StoryClient,
-  StoryConfig,
 } = require("@story-protocol/core-sdk");
 const { http } = require("viem");
+const { mintNFTOnBlockchain } = require("./blockchain.js");
 const { createHash } = require("crypto");
 const { uploadJSONToIPFS } = require("../utils/uploadToIPFS.js");
 
 const storyProviderUrl = process.env.STORY_RPC_URL;
 const privateKey = process.env.PRIVATE_KEY;
-const NFTContractAddress = process.env.NFT_CONTRACT_ADDRESS;
+const NFTContractAddress = process.env.CONTRACT_ADDRESS;
+const userWalletAddress = process.env.WALLET_ADDRESS;
 
 // Function to register an existing NFT as IP on Story Protocol
 const mintAndRegisterIP = async (contentToRegister) => {
-  console.log("content to register", contentToRegister);
   try {
     const provider = new ethers.JsonRpcProvider(storyProviderUrl);
     const wallet = new ethers.Wallet(privateKey, provider);
 
+    // Make sure the account is correctly configured
+    const account = {
+      address: wallet.address,
+      signTypedData: async (domain, types, message) =>
+        wallet._signTypedData(domain, types, message),
+      signTransaction: async (transaction) =>
+        wallet.signTransaction(transaction),
+      sendTransaction: async (signedTransaction) =>
+        provider.sendTransaction(signedTransaction),
+    };
+
     const client = new StoryClient({
-      wallet,
+      account, // Ensure that account with the signer is attached
       transport: http(storyProviderUrl),
       chainId: "iliad",
     });
 
-    const removeCircular = () => {
-      const seen = new WeakSet();
-      return (key, value) => {
-        if (typeof value === "object" && value !== null) {
-          if (seen.has(value)) {
-            return;
-          }
-          seen.add(value);
-        }
-        return value;
-      };
-    };
-
-    //schema example
-    const ipMetadata = client.ipAsset.generateIpMetadata(
-      contentToRegister
-      //{
-      //   title: "My IP Asset",
-      //   description: "This is a test IP asset",
-      //   attributes: [
-      //     {
-      //       key: "Rarity",
-      //       value: "Legendary",
-      //     },
-      //   ],
-      //}
-    );
+    // Prepare the IP metadata and NFT metadata
+    const ipMetadata = client.ipAsset.generateIpMetadata({
+      title: "My IP Asset",
+      description: "This is a test IP asset",
+      attributes: [
+        {
+          key: "Rarity",
+          value: "Legendary",
+        },
+      ],
+    });
 
     const nftMetadata = {
       name: "NFT representing ownership of IP Asset",
       description: "This NFT represents ownership of an IP Asset",
     };
 
+    // Upload metadata to IPFS
     const ipIpfsHash = await uploadJSONToIPFS(ipMetadata);
     const ipHash = createHash("sha256")
       .update(JSON.stringify(ipMetadata))
       .digest("hex");
+
     const nftIpfsHash = await uploadJSONToIPFS(nftMetadata);
     const nftHash = createHash("sha256")
       .update(JSON.stringify(nftMetadata))
       .digest("hex");
 
-    const response = await client.assets.register({
+    // Mint the NFT on blockchain
+    const data = await mintNFTOnBlockchain(
+      wallet,
+      ipIpfsHash,
+      " ",
+      nftMetadata.description
+    );
+    const tokenId = data.tokenId.toString();
+
+    // Register the IP asset with Story Protocol
+    const response = await client.ipAsset.register({
       nftContract: NFTContractAddress,
       tokenId: tokenId,
       pilType: PIL_TYPE.NON_COMMERCIAL_REMIX,
-      mintingFee: 0, // empty - doesn't apply
-      currency: AddressZero, // empty - doesn't apply
+      mintingFee: 0, // No fee
+      currency: AddressZero, // No currency
       ipMetadata: {
         ipMetadataURI: `https://ipfs.io/ipfs/${ipIpfsHash}`,
         ipMetadataHash: `0x${ipHash}`,
         nftMetadataURI: `https://ipfs.io/ipfs/${nftIpfsHash}`,
         nftMetadataHash: `0x${nftHash}`,
       },
-      txOptions: { waitForTransaction: true },
+      txOptions: {
+        gasLimit: ethers.utils.hexlify(21000), // Set gas limit
+        gasPrice: ethers.utils.parseUnits("50", "gwei"), // Set gas price
+        waitForTransaction: true,
+      },
     });
 
     console.log(
